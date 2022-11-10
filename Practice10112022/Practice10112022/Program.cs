d4b171127fa804968068eddf47a6201430841496
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-10 08:17:07 +0530

    [Afzal] Added: Even or Odd

--- Practice10112022/Practice10112022/Program.cs
+++ Practice10112022/Practice10112022/Program.cs
@@ -6,7 +6,17 @@ namespace Practice10112022
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Practice Problem");
+            Console.WriteLine("Enter Options"+"\n"+
+                                "1.Even or Odd");
+            int options = Convert.ToInt32(Console.ReadLine());
+            switch (options)
+            {
+                case 1: 
+                    EvenorOdd evenorOdd = new EvenorOdd();
+                    evenorOdd.EvenOdd();
+                    break;
+            }
         }
     }
 }
